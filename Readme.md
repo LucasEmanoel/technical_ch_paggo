d9ef007f8e9347db67ab12cca256771a863fddbd
Lucas Emanoel <lucas2014.barros@gmail.com>
2025-04-30 15:38:04 -0300

    feat: project setup - basic model mapping and database containers

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Setup do projeto
+
+## Ambiente Virtual Python
+
+embora possa optar por utilizar o interpretador global, recomendo criar um venv para esse projeto.
+basta simplesmente ir para a pasta raiz do projeto (`./technical_ch_paggo`) e rodar o seguinte comando :
+`python3 -m venv .venv` 
+
+1. python3 : interpretador do python
+2. -m venv : modulo de criação de ambientes virtuais
+3. .venv : pasta que será criada.
+
+## Instalação de Dependencias
+
+
+## Criação do banco de dados
+Nessa etapa será realizada pelo docker, iremos configurar dois containers para rodar os dois bancos de dados em paralelo. 
+
+ 
---
+++ api/app/model/DataModel.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from sqlalchemy import Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class Data(Base):
+    __tablename__ = "data"
+
+    id:                 Mapped[int] = mapped_column(primary_key=True)
+    timestamp:          Mapped[datetime] = mapped_column(DateTime, default=datetime.now(datetime.timezone.utc))
+    wind_spped:         Mapped[float] = mapped_column(Float)
+    power:              Mapped[float] = mapped_column(Float)
+    ambient_temprature: Mapped[float] = mapped_column(Float)
+    
+    
